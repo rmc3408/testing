ce4b16d8618d4e964f84958ef02cc52fb159712e
rmc3408 <rmc3408@protonmail.com>
2022-02-10 10:37:38 -0500

    funfunfunction is horrible

---
+++ funfunfunctions/orders.js
@@ -0,0 +1,17 @@
+const someOrder = {
+    items: [
+        { name: 'Dragon food', price: 8, quantity: 8 },
+        { name: 'Dragon cage (small)', price: 800, quantity: 2 },
+        { name: 'Shipping', price: 40, shipping: true },
+    ],
+};
+
+const orderTotal = (order) => {
+    totalNormalItems = order.items.filter((x) => !x.shipping).reduce((prev, cur) => prev + (cur.quantity * cur.price), 0);
+    shippingItem = order.items.find((x) => !!x.shipping);
+    shipping = totalNormalItems > 1000 ? 0 : shippingItem.price;
+    return totalNormalItems + shipping;
+};
+
+const result = orderTotal(someOrder);
+// results expected is 1664
